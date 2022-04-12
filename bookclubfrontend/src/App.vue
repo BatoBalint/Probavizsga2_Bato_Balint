a25a29ad88341265b20e3cf121cc1cfc5cd4a17b
BatoBalint <balint.bato@gmail.com>
2022-04-12 13:57:49 +0200

    form majdnem mukodik

--- bookclubfrontend/src/App.vue
+++ bookclubfrontend/src/App.vue
@@ -4,22 +4,31 @@
     <h1>Members</h1>
   </div>
   <div class="row">
-    <div class="col-sm-3">
-      <div class="card" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="...">
+    <div v-for="member in members" :key="member.id" class="col-sm-12 col-md-6 col-lg-4 my-5">
+      <div class="card text-dark">
+        <img v-if="member.gender == 'M'" src="./assets/male.png" class="card-img-top" alt="male image">
+        <img v-else-if="member.gender == 'F'" src="./assets/female.png" class="card-img-top" alt="female image">
+        <img v-else src="./assets/other.png" class="card-img-top" alt="unidentified gender image">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">{{ member.name }}</h5>
+          <p class="card-text">Text</p>
         </div>
       </div>
     </div>
+  </div>
+  <div class="row my-5">
+    <h2>New member</h2>
+    <Form />
   </div>
 </div>
 </template>
 
 <script>
+    import Form from './components/Form.vue'
   export default {
+    components: {
+      Form
+    },
     data() {
       return {
         members: {}
@@ -29,7 +38,7 @@
       getData() {
         fetch('http://localhost:8000/api/member')
         .then(response => response.json())
-        .then(data => members = data);
+        .then(data => this.members = data);
       }
     },
     mounted() {
---
+++ bookclubfrontend/src/components/Form.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            <form @submit.prevent>
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-control" v-model="nameInput">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Gender</label>
+                    <input type="text" class="form-control" v-model="genderInput">
+                </div>
+                <div class="mb-3">
+                    <label class="form-check-label">Birth Date</label>
+                    <input type="date" class="form-control" v-model="birthDateInput">
+                </div>
+                <div>
+                    <button class="btn btn-success" @click="submitBtnClick">Submit</button>
+                </div>
+            </form>  
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        member: {},
+        nameInput: "",
+        genderInput: "",
+        birthDateInput: ""
+      }
+    },
+    methods: {
+        submitBtnClick() {
+            alert(this.nameInput + " , " + this.genderInput + " , " + this.birthDateInput);
+        },
+      getData() {
+        fetch('http://localhost:8000/api/member')
+        .then(response => response.json())
+        .then(data => this.members = data);
+      }
+    },
+    mounted() {
+      this.getData();
+    }
+  }
+</script>
+
+<style>
+
+</style>
